090911cc9c07a7ed332325d68aa471e950a27139
ArianGhatrehSamani <arianghatreh@gmail.com>
2022-07-16 13:08:21 +0430

    Added event classes

--- Arkitect/src/core/Base.h
+++ Arkitect/src/core/Base.h
@@ -1,3 +1,5 @@
 #pragma once
 
 #include "Log.h"
+
+#define RKT_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
--- Arkitect/src/core/EntryPoint.h
+++ Arkitect/src/core/EntryPoint.h
@@ -3,9 +3,14 @@
 #include "Base.h"
 #include "Application.h"
 
+#include "events/KeyEvent.h"
+
 extern Arkitect::Application* Arkitect::CreateApplication();
 
 int main(int argc, char** argv) {
+
+	Arkitect::KeyPressedEvent k(Arkitect::Key::K);
+	std::cout << k.ToString();
 
 	Arkitect::Log::Init();
 
